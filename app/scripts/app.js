b6c0ca43ed947370dee6ed75e8ce9d5286d27c21
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-05-26 23:56:19 +0200

    First commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,34 @@
-var app = angular.module('personal-blog', ['prismic.io']);
+var app = angular.module('personal-blog', [
+	'ui.router',
+	'ngRoute',
+	'ngAnimate',
+	'prismic.io',
+	'personal-blog.controllers'
+]);
 
-app.config(function(PrismicProvider) {
-    PrismicProvider.setApiEndpoint('https://personal-blog.prismic.io/api');
-    PrismicProvider.setAccessToken('');
-    PrismicProvider.setClientId('');
-    PrismicProvider.setClientSecret('');
-    PrismicProvider.setLinkResolver(function(ctx, doc) {
-        return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
-    });
+app.config(function($stateProvider, $urlRouterProvider, PrismicProvider) {
+	'use strict';
+
+	$urlRouterProvider
+	.otherwise('/home');
+
+	$stateProvider
+	.state('home', {
+		url: '/home',
+		views: {
+			'mainView': { controller: 'AppCtrl'}
+		}
+	});
+	PrismicProvider.setApiEndpoint('https://personal-blog.prismic.io/api');
+	PrismicProvider.setAccessToken('');
+	PrismicProvider.setClientId('');
+	PrismicProvider.setClientSecret('');
+	PrismicProvider.setLinkResolver(function(ctx, doc) {
+		return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
+	});
 });
+
+app.run(function($rootScope) {
+	$rootScope.mode = 'dev';
+});
+
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,5 +1,6 @@
 angular.module('portail-qualif.controllers', [])
 	.controller('AppCtrl', ['Prismic', function(Prismic) {
+		'use strict';
 	    var self = this;
 	    Prismic.all().then(function(data) {
 	        self.data = data;
